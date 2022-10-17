df954bf9d6d890151a7f3c906837951119ac31c6
Алексей Зигунов <alexey@MacBook-Pro-Aleksej.local>
2022-10-17 14:03:18 +0300

    main page

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// const cartCounter = document.getElementById("counter-number");
+
+// // change font in cart 
+// function changeCartFont() {
+//     if (+cartCounter.textContent > 9) {
+//         cartCounter.style.fontSize = "10px";
+//     } else {
+//         cartCounter.style.fontSize = "12px";
+//     }
+// };
+
+// changeCartFont();
+
+// let observer = new MutationObserver(changeCartFont);
+// observer.observe(cartCounter, {
+//     attributes: true,
+// });
+
+
+// setTimeout(() => cartCounter.textContent = "1", 2000);
+
+
+const shopButton = document.querySelector(".shopButton");
+const menu = document.querySelector(".menu");
+
+function toggleActivity() {
+    if (menu.classList.contains('menu-active')) {
+        menu.classList.remove('menu-active');
+        menu.classList.add('menu-hidden');
+    } else {
+        menu.classList.remove('menu-hidden');
+        menu.classList.add('menu-active');
+    }
+}
+
+console.log(menu);
+shopButton.addEventListener('click', toggleActivity);
